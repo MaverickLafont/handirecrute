ed0ab3bd2cfe836c5ca9bf6a2db14c41e4bfaf2d
Maverick <mavericklafont@gmail.com>
2021-03-09 18:30:59 +0100

    force l'utilisateur à activer son compte sinon aucun acces sur le site

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * HomeController constructor.
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Si l'utilisateur est login mais n'a pas validé son compte on le
+     * renvoie sur la page de confirmation. Sinon on le laisse acceder a
+     * l'accueil
+     *
      * @Route("/", name="home")
      */
     public function index(): Response
     {
+        if($this->getUser() !== null){
+            $user = $this->userRepository->findOneBy(['email' => $this->getUser()->getUsername()]);
+
+            // Si l'utilisateur est connecté en ayant validé son compte
+            if($user->isVerified() === true){
+                return $this->render('home/index.html.twig');
+            }
+            // Si l'utilisateur est connecté sans avoir validé son compte
+            return $this->render('registration/confirm.html.twig', [
+                'username' => $user->getUsername()
+            ]);
+        }
+
+        // Si l'utilisateur n'est pas connecté
         return $this->render('home/index.html.twig');
     }
 }
